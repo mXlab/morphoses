931abf919a206d96fd8022bfd264330fda5cbf61
Sofian Audry <info@sofianaudry.com>
2018-08-22 13:30:46 -0400

    Receiving position+quat through OSC and sending it to CSV file.

---
+++ Python/mp_unity_receive.py
@@ -0,0 +1,38 @@
+import argparse
+import math
+import csv
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def handle_data(unused_addr, x, y, qx, qy, qz, qw):
+    csv_writer.writerow([x, y, qx, qy, qz, qw])
+
+if __name__ == "__main__":
+    # Create parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("output_file", type=str, help="CSV output file")
+    parser.add_argument("-i", "--ip", default="127.0.0.1", help="Specify the ip address to listen on.")
+    parser.add_argument("-p", "--port", default="8767", type=int, help="Specify the port number to listen on.")
+
+    # Parse arguments.
+    args = parser.parse_args()
+
+    # Create CSV file.
+    csv_file = open(args.output_file, "w")
+    csv_writer = csv.writer(csv_file)
+#    csv_writer = csv.DictWriter(csv_file, fieldnames=['x', 'y', 'qx', 'qy', 'qz', 'qw'])
+#    csv_writer.writeheader()
+
+    # Create OSC dispatcher.
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("/morphoses/data", handle_data)
+#    dispatcher.map("/volume", print_volume_handler, "Volume")
+#    dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+    # Launch OSC server.
+    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
--- Unity/MorphosisBall/Assets/Scripts/SendOSC.cs
+++ Unity/MorphosisBall/Assets/Scripts/SendOSC.cs
@@ -7,22 +7,24 @@ public class SendOSC : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 
 	  OscMessage message = new OscMessage();
 
-        message.address = "/quat";
-        message.values.Add(transform.rotation.x);
+    message.address = "/morphoses/data";
+		// Position.
+		message.values.Add(transform.position.x);
+		message.values.Add(transform.position.z);
+		// Quaternions.
+    message.values.Add(transform.rotation.x);
 		message.values.Add(transform.rotation.y);
 		message.values.Add(transform.rotation.z);
 		message.values.Add(transform.rotation.w);
-        osc.Send(message);
-
-    }
-
+    osc.Send(message);
+  }
 
 }
