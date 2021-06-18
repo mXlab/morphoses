75f3d1ab934dfac3f811e14780a4d29e7f973ad6
Sofian Audry <info@sofianaudry.com>
2021-06-18 12:36:01 -0400

    Reorganized IMU board code based on rework of main board code.

---
+++ Arduino/MorphosesIMUBoard/IMU.h
@@ -0,0 +1,50 @@
+#include <SparkFun_BNO080_Arduino_Library.h>
+
+BNO080 imu;
+
+boolean imuInitialized = false;
+
+boolean imuIsInitialized() { 
+  return imuInitialized; 
+}
+
+void initImu() {
+  if (!imu.begin()) {
+    Serial.println("BNO080 not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
+    bndl.add("/error");
+    sendOscBundle();
+    blinkIndicatorLed(1000, 0.1);
+  }
+  else {
+    bndl.add("/ready");
+    Wire.setClock(400000); //Increase I2C data rate to 400kHz
+    imu.enableRotationVector(50); //Send data update every 50ms
+//      imu.enableMagnetometer(50);
+    imuInitialized = true;
+  }
+  // Add details and send.
+  bndl.add(boardName).add("imu-i2c");
+  sendOscBundle();
+}
+
+void sleepImu() {
+  imu.modeSleep();
+}
+
+void wakeImu() {
+  imu.modeOn();
+}
+
+bool processImu() {
+  // Get data.
+  bool dataAvailable = imu.dataAvailable();
+
+  // Send data over OSC.
+  if (dataAvailable && sendOSC)
+  {
+    bndl.add("/quat").add(imu.getQuatI()).add(imu.getQuatJ()).add(imu.getQuatK()).add(imu.getQuatReal());
+//    bndl.add("/euler").add((float)degrees(imu.getRoll())).add((float)degrees(imu.getPitch())).add((float)degrees(imu.getYaw()));
+//      bndl.add("/mag").add(imu.getMagX()).add(imu.getMagY()).add(imu.getMagZ());
+  }
+  return dataAvailable;
+}
--- Arduino/MorphosesIMUBoard/MorphosesIMUBoard.ino
+++ Arduino/MorphosesIMUBoard/MorphosesIMUBoard.ino
@@ -8,7 +8,7 @@
  * INSTRUCTIONS: Copy Config.h.default to Config.h and adjust according to 
  * your own Wifi setup.
  * 
- * (c) 2018-2020 Sofian Audry, Martin Peach
+ * (c) 2018-2021 Sofian Audry, Pierre Gaudet, Martin Peach
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,281 +25,95 @@
  */
  #include "Config.h"
 
- #include <driver/adc.h>
+// Includes /////////////////////////////////////
 
-#include <SparkFun_BNO080_Arduino_Library.h>
-
+// Configuration file.
+#include "Config.h"
+#include "Utils.h"
+#include "OTA.h"
+#include "Comm.h"
+#include "IMU.h"
 #include <Chrono.h>
 
-#include <OSCBundle.h>
+// Variables & Objects //////////////////////////
 
-#ifdef ARDUINO_ARCH_ESP32
-#include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
-#endif
+Chrono sendDataChrono;
 
-#include <WiFiUdp.h>
-#include <SLIPEncodedSerial.h>
-SLIPEncodedSerial SLIPSerial(Serial);
+void setup()
+{
+  // Start I2C.
+  Wire.begin();
 
-#define WIFI_CONNECTION_TIMEOUT 5000
-
-BNO080 imu;
-WiFiUDP udp;
-OSCBundle bndl;
-
-IPAddress destIP(DEST_IP_0, DEST_IP_1, DEST_IP_2, DEST_IP_3); // remote IP
-IPAddress broadcastIP(DEST_IP_0, DEST_IP_1, DEST_IP_2, 255); // broadcast
-
-char packetBuffer[128];
-
-void sendOscBundle(boolean broadcast=false);
-void blinkIndicatorLed(unsigned long period, float pulseProportion=0.5, int nBlinks=1);
-
-bool imuInitialized = false;
-
-//int sensorValue = 0;
-//int analogPin = 4;
-
-// Chronometer to control when the IMU data is fetched.
-Chrono imuDataChrono;
-
-void setup() {
+  // Start serial.
   Serial.begin(115200);
-
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  // Initialize Wifi and UDP.
+   // Initialize Wifi and UDP.
   initWifi();
 
-  Wire.begin();
-
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  //adc1_config_width(ADC_WIDTH_BIT_12);
-  //adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
-  //float value = (float)adc1_get_raw(ADC1_CHANNEL_0);
+  // Initialize OTA.
+  initOTA(boardName);
 }
 
-void loop() {
-  //sensorValue = analogRead(analogPin);
-  //Serial.println(sensorValue);
-  
-  //int value = adc1_get_raw(ADC1_CHANNEL_0);
-  //Serial.println(value);
-  //delay(50);
-  
+void loop()
+{
+  // Update OTA.
+  updateOTA();  
+   
   // Check connection status: reconnect if connection lost.
-  if (WiFi.status() != WL_CONNECTED)
+  if (!wifiIsConnected())
     initWifi();
 
   // Init IMU if not already initialized.
-  if (!imuInitialized)
-    initIMU();
+  if (!imuIsInitialized())
+    initImu();
   
   // Check for incoming messages.
-  receiveMessage();
-  
-  // Send IMU.
-  if (imuDataChrono.hasPassed(SEND_DATA_INTERVAL)) {
-    imuDataChrono.restart();
-    processImu();
+  OSCMessage message;
+  if (receiveMessage(message))
+    processMessage(message);
+
+  // Send messages.
+  if (sendDataChrono.hasPassed(SEND_DATA_INTERVAL)) {
+    sendData();
+    sendDataChrono.restart();
   }
 }
 
-void receiveMessage() {
-  // if there's data available, read a packet
-  int packetSize = udp.parsePacket();
+void sendData() {
+  // Process motor ticks only if IMU ready.
+  processImu();
 
-  if (packetSize)
-  {
-    if (OSCDebug) {
-      Serial.print("Received packet of size ");
-      Serial.println(packetSize);
-      Serial.print("From ");
-    }
-    IPAddress remote = udp.remoteIP();
-    if (OSCDebug) {
-      for (int i = 0; i < 4; i++)
-      {
-        Serial.print(remote[i], DEC);
-        if (i < 3)
-        {
-          Serial.print(".");
-        }
-      }
-      Serial.print(", port ");
-      Serial.println(udp.remotePort());
-    }
-
-    // Read the packet
-    OSCMessage messIn;
-    while (packetSize--) messIn.fill(udp.read());
-
-    switch(messIn.getError()) {
-      case  OSC_OK:
-        int messSize;
-        if (OSCDebug) Serial.println("no errors in packet");
-        messSize = messIn.size();
-        if (SerialDebug) {
-          Serial.print("messSize: ");
-          Serial.println(messSize);
-        }
-        if (OSCDebug) {
-          char addressIn[64];
-          messSize = messIn.getAddress(addressIn, 0, 64);
-          Serial.print("messSize: ");
-          Serial.println(messSize);
-          Serial.print("address: ");
-          Serial.println(addressIn);
-        }
-        processMessage(messIn);
-
-        break;
-      case BUFFER_FULL:
-        if (OSCDebug) Serial.println("BUFFER_FULL error");
-        break;
-      case INVALID_OSC:
-        if (OSCDebug) Serial.println("INVALID_OSC error");
-        break;
-      case ALLOCFAILED:
-        if (OSCDebug) Serial.println("ALLOCFAILED error");
-        break;
-      case INDEX_OUT_OF_BOUNDS:
-        if (OSCDebug) Serial.println("INDEX_OUT_OF_BOUNDS error");
-        break;
-    }
-  } //if (packetSize)
+  // Send OSC bundle.
+  sendOscBundle();
 }
 
 void processMessage(OSCMessage& messIn) {
   // This message assigns destination IP to the remote IP from which the OSC message was sent.
   if (messIn.fullMatch("/bonjour")) {
-    if (OSCDebug) Serial.println("Init IP");
+    if (DEBUG_MODE) Serial.println("Init IP");
     destIP = udp.remoteIP();
-    bndl.add("/imu/bonjour");
-    sendOscBundle();
+    bndl.add("/bonjour").add(boardName);
   }
-}
-
-void processImu() {
-  if (imu.dataAvailable() == true)
-  {
-//    float quatI = imu.getQuatI();
-//    float quatJ = imu.getQuatJ();
-//    float quatK = imu.getQuatK();
-//    float quatReal = imu.getQuatReal();
-//    Serial.println(quatI);
-//    Serial.println(quatJ);
-//    Serial.println(quatK);
-//    Serial.println(quatReal);
-////    float quatRadianAccuracy = imu.getQuatRadianAccuracy();
-
-    if (sendOSC) {
-      bndl.add("/quat").add(imu.getQuatI()).add(imu.getQuatJ()).add(imu.getQuatK()).add(imu.getQuatReal());
-      bndl.add("/euler").add((float)degrees(imu.getRoll())).add((float)degrees(imu.getPitch())).add((float)degrees(imu.getYaw()));
-      sendOscBundle();
+  
+  // Stream OSC messages ON/OFF.
+  else if (messIn.fullMatch("/stream")) {
+    if (DEBUG_MODE) Serial.println("STREAM");
+    if (argIsNumber(messIn, 0)) {
+      if (DEBUG_MODE) Serial.print("stream value ");
+      int32_t val = getArgAsInt(messIn, 0);
+      if (DEBUG_MODE) Serial.println(val);
+      boolean stream = (val != 0);
+      // If new value, enable/disable OSC & wake/sleep IMU.
+      if (sendOSC != stream) {
+        sendOSC = stream;
+        if (sendOSC)
+          wakeImu();
+        else
+          sleepImu();
+      }
     }
-  }
-}
-
-void initIMU() {
-  if (!imuInitialized)
-  {
-    if (!imu.begin()) {
-      Serial.println("BNO080 not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
-      bndl.add("/imu/i2c/error");
-      sendOscBundle();
-      blinkIndicatorLed(1000, 0.1);
-    }
-    else {  
-      bndl.add("/imu/i2c/ok");
-      sendOscBundle();
-      Wire.setClock(400000); //Increase I2C data rate to 400kHz
-      imu.enableRotationVector(50); //Send data update every 50ms
-      imuInitialized = true;
-    }
-  }
-}
-
-void initWifi()
-{
-  /**
-   * Set up an access point
-   * @param ssid          Pointer to the SSID (max 63 char).
-   * @param passphrase    (for WPA2 min 8 char, for open use NULL)
-   * @param channel       WiFi channel number, 1 - 13.
-   * @param ssid_hidden   Network cloaking (0 = broadcast SSID, 1 = hide SSID)
-   */
-  // now start the wifi
-  WiFi.mode(WIFI_AP_STA);
-#if AP_MODE
-  /* You can remove the password parameter if you want the AP to be open. */
-  if (!WiFi.softAP(ssid, password)) {
-    while(1); // Loop forever if setup didn't work
-  }
-
-  IPAddress myIP = WiFi.softAPIP();
-
-#else
-  WiFi.begin(ssid, password);
-
-  // Wait for connection to complete.
-  unsigned long startMillis = millis();
-  while (WiFi.status() != WL_CONNECTED && 
-         millis() - startMillis < WIFI_CONNECTION_TIMEOUT) {
-    Serial.print(".");
-    blinkIndicatorLed(500);
-  }
-
-  // If still not connected, restart the board.
-  if (WiFi.status() != WL_CONNECTED) {
-    blinkIndicatorLed(100, 0.7, 20);
-    ESP.restart();
-  }
-
-  IPAddress myIP = WiFi.localIP();
-#endif
-  Serial.println("IP: ");
-  Serial.println(myIP);
-
-  if (!udp.begin(localPort)) {
-    while(1); // Loop forever if setup didn't work
-  }
-  Serial.println("Done");
-
-  // Broadcast IP address.
-  bndl.add("/imu/ip").add(myIP[3]);
-  sendOscBundle(true);
-}
-
-void sendOscBundle(boolean broadcast) {
-  if (sendOSC) {
-
-    if (useUdp) {
-      udp.beginPacket(broadcast ? broadcastIP : destIP, destPort);
-      bndl.send(udp); // send the bytes to the SLIP stream
-      udp.endPacket(); // mark the end of the OSC Packet
-    }
-    else {
-      SLIPSerial.beginPacket();
-      bndl.send(SLIPSerial); // send the bytes to the SLIP stream
-      SLIPSerial.endPacket(); // mark the end of the OSC Packet
-    }
-  }
-  bndl.empty(); // empty the bundle to free room for a new one
-}
-
-void blinkIndicatorLed(unsigned long period, float pulseProportion, int nBlinks) {
-  while (nBlinks--) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay((unsigned long) (period * pulseProportion));
-    digitalWrite(LED_BUILTIN, LOW);
-    delay((unsigned long) ((period * (1-pulseProportion))));
   }
 }
