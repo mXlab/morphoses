cf6f3cf8e6c4cbc276caf70f7f7fb8d024b89440
Sofian Audry <info@sofianaudry.com>
2021-06-24 13:33:02 -0400

    Cleanup in comments.

--- Arduino/MorphosesMainBoard/Pixels.h
+++ Arduino/MorphosesMainBoard/Pixels.h
@@ -11,16 +11,20 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 //**************************************************************
 
 void initPixels() {
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)  
+  // INITIALIZE NeoPixel strip object (REQUIRED)  
+  pixels.begin(); 
 }
 
 
 void setPixels(int r, int g, int b) {
-  //neopixel functions:
-  pixels.clear(); // Set all pixel colors to 'off'
+  // Set all pixel colors to 'off'
+  pixels.clear();
+
+  // Write all pixels.
   for (int i=0; i<pixels.numPixels(); i++)
     pixels.setPixelColor(i, pixels.Color(r, g, b));
   portDISABLE_INTERRUPTS();
   pixels.show();   // Send the updated pixel colors to the hardware.
   portENABLE_INTERRUPTS();
+  pixels.show();   
 }
