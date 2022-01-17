8774dbd20611135d42d45d6bc5e928acf520857e
Sofian Audry <info@sofianaudry.com>
2022-01-17 11:17:03 -0500

    Renamed motor constants with better names.

--- Arduino/MorphosesMainBoard/Motors.h
+++ Arduino/MorphosesMainBoard/Motors.h
@@ -8,8 +8,8 @@
 // Dynamixel parameters ***********************************
 #define DXL_SERIAL   Serial1
 const uint8_t DXL_DIR_PIN = 5; // DYNAMIXEL Shield DIR PIN
-const uint8_t DXL_ID = 1; //motor for rolling
-const uint8_t DXL_ID2 = 2; // motor for steering left right
+const uint8_t DXL_ID_SPEED = 1; //motor for rolling
+const uint8_t DXL_ID_STEER = 2; // motor for steering left right
 const float DXL_PROTOCOL_VERSION = 2.0;
 
 // Dynamixel motor control object.
@@ -32,27 +32,27 @@ void initMotors() {
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
   // Turn off torque when configuring items in EEPROM area
-  dxl.torqueOff(DXL_ID);
-  dxl.torqueOff(DXL_ID2);
-  dxl.setOperatingMode(DXL_ID, OP_VELOCITY); // rolling
-  dxl.setOperatingMode(DXL_ID2, OP_POSITION); // steering left right
-  dxl.torqueOn(DXL_ID);
-  dxl.torqueOn(DXL_ID2);
+  dxl.torqueOff(DXL_ID_SPEED);
+  dxl.torqueOff(DXL_ID_STEER);
+  dxl.setOperatingMode(DXL_ID_SPEED, OP_VELOCITY); // rolling
+  dxl.setOperatingMode(DXL_ID_STEER, OP_POSITION); // steering left right
+  dxl.torqueOn(DXL_ID_SPEED);
+  dxl.torqueOn(DXL_ID_STEER);
 
-  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID, profile_acceleration_rolling);
-  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID, profile_velocity_rolling);
+  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_SPEED, profile_acceleration_rolling);
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_SPEED, profile_velocity_rolling);
 
-  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID2, profile_acceleration_steering);
-  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID2, profile_velocity_steering);
+  dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_STEER, profile_acceleration_steering);
+  dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_STEER, profile_velocity_steering);
 }
 
 void setMotorsPower(bool on) {
   if (on) {
-    dxl.torqueOn(DXL_ID);
-    dxl.torqueOn(DXL_ID2);
+    dxl.torqueOn(DXL_ID_SPEED);
+    dxl.torqueOn(DXL_ID_STEER);
   } else {
-    dxl.torqueOff(DXL_ID);
-    dxl.torqueOff(DXL_ID2);
+    dxl.torqueOff(DXL_ID_SPEED);
+    dxl.torqueOff(DXL_ID_STEER);
   }
 }
 
@@ -67,9 +67,11 @@ int safeRemapNorm(float unitVal, int maxRange, int midPoint=0) {
 }
 
 void setMotorsSpeed(float speed) {
-  dxl.setGoalVelocity(DXL_ID, safeRemapNorm(speed, MOTORS_SPEED_MAX), UNIT_RAW); // +n=CCW, -n=CW
+  dxl.setGoalVelocity(DXL_ID_SPEED, safeRemapNorm(speed, MOTORS_SPEED_MAX), UNIT_RAW); // +n=CCW, -n=CW
 }
 
 void setMotorsSteer(float steer) {
-  dxl.setGoalPosition(DXL_ID2, safeRemapNorm(steer, MOTORS_STEER_MAX, MOTORS_STEER_MIDDLE), UNIT_DEGREE);
+  dxl.setGoalPosition(DXL_ID_STEER, safeRemapNorm(steer, MOTORS_STEER_MAX, MOTORS_STEER_MIDDLE), UNIT_DEGREE);
+}
+
 }
