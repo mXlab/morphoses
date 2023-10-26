ce86df392ebab021bb9d9e5048254a9d06e0a343
etiennemontenegro <43181133+etiennemontenegro@users.noreply.github.com>
2023-10-26 16:29:15 -0400

    added import guards

--- Arduino/morphose_platformio/src/Globals.h
+++ Arduino/morphose_platformio/src/Globals.h
@@ -1,3 +1,6 @@
+#ifndef MORPHOSE_GLOBAL_H
+#define MORPHOSE_GLOBAL_H
+
 // Local (input) port is always 8000.
 #define LOCAL_PORT 8000
 
@@ -16,15 +19,13 @@
 #define DEST_IP_2 0
 #define DEST_IP_3 100
 
-// MQTT settings.
-#define MQTT_BROKER "192.168.0.200"
-#define MQTT_BROKER_PORT 1883
+
 
 // RTLS IDs.
-const char *ROBOT_RTLS_IDS[N_ROBOTS] = { "1a1e", "0f32", "5b26" };
-char ROBOT_RTLS_MQTT_ADDRESS[N_ROBOTS][32]; // Internal use to keep full MQTT addresses.
+static const char *ROBOT_RTLS_IDS[N_ROBOTS] = { "1a1e", "0f32", "5b26" };
+static char ROBOT_RTLS_MQTT_ADDRESS[N_ROBOTS][32]; // Internal use to keep full MQTT addresses.
 
-char ROBOT_CUSTOM_MQTT_ADDRESS[32];
+static char ROBOT_CUSTOM_MQTT_ADDRESS[32];
 
 // Neopixel settings.
 #define NUM_PIXELS 32
@@ -35,3 +36,5 @@ char ROBOT_CUSTOM_MQTT_ADDRESS[32];
 //  /* TOP */    { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, -1 },
 //  /* BOTTOM */ { 24, 25, 26, 27, 28, 29, 30, 31, -1 }
 //};
+
+#endif
