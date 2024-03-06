218b957fb8aefca5c53360562d7061ce3c3a5009
Sofian Audry <info@sofianaudry.com>
2024-03-05 19:31:36 -0500

    modified startup scripts

---
+++ startup/behaviors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python /home/morphoses/morphoses/Python/ml/morphoses.py /home/morphoses/morphoses/Python/ml/behaviors/immobilism.yml --settings-file /home/morphoses/morphoses/Python/ml/settings.yml
