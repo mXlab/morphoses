f79dbdd637055343e1b84ffbed3eb216068851a9
Sofian Audry <info@sofianaudry.com>
2024-04-10 11:47:48 -0400

    Added thread safety to engine using semaphore (untested).

--- Arduino/morphose_platformio/src/hardware/Engine.cpp
+++ Arduino/morphose_platformio/src/hardware/Engine.cpp
@@ -13,6 +13,9 @@
 
 namespace motors {
 
+    // Mutex for async Dynamixel communication.
+    SemaphoreHandle_t dxlMutex = NULL;
+
     // Dynamixel parameters ***********************************
     const uint8_t DXL_DIR_PIN = 5;  // DYNAMIXEL Shield DIR PIN
     const uint8_t DXL_ID_SPEED = 1;  // motor for rolling
@@ -34,67 +37,83 @@ namespace motors {
 
     float currentSpeed;
     float currentSteer;
-    float engineSpeed;
-    float engineSteer;
     bool enginePower;
-    bool newEngineData;
+
+    bool lockMutex() {
+        return (xSemaphoreTake (dxlMutex, portMAX_DELAY));
+    }
+
+    void unlockMutex() {
+        xSemaphoreGive(dxlMutex);  // release the mutex
+    }
 
     void initialize() {
+
+        // Create mutex.
+        dxlMutex = xSemaphoreCreateMutex();
+
         enginePower = false;
-        newEngineData = false;
 
         // Set Port baudrate to 57600bps for DYNAMIXEL motors.
-        dxl.begin(57600);
-        // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
-        dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+        if (lockMutex()) {
+            dxl.begin(57600);
+            // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+            dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-        // Turn off torque when configuring items in EEPROM area
-        dxl.torqueOff(DXL_ID_SPEED);
-        dxl.torqueOff(DXL_ID_STEER);
-        dxl.setOperatingMode(DXL_ID_SPEED, OP_VELOCITY);  // rolling
-        dxl.setOperatingMode(DXL_ID_STEER, OP_POSITION);  // steering left right
-        dxl.torqueOn(DXL_ID_SPEED);
-        dxl.torqueOn(DXL_ID_STEER);
+            // Turn off torque when configuring items in EEPROM area
+            dxl.torqueOff(DXL_ID_SPEED);
+            dxl.torqueOff(DXL_ID_STEER);
+            dxl.setOperatingMode(DXL_ID_SPEED, OP_VELOCITY);  // rolling
+            dxl.setOperatingMode(DXL_ID_STEER, OP_POSITION);  // steering left right
+            dxl.torqueOn(DXL_ID_SPEED);
+            dxl.torqueOn(DXL_ID_STEER);
 
-        dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_SPEED, profile_acceleration_rolling);
-        dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_SPEED, profile_velocity_rolling);
+            dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_SPEED, profile_acceleration_rolling);
+            dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_SPEED, profile_velocity_rolling);
 
-        dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_STEER, profile_acceleration_steering);
-        dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_STEER, profile_velocity_steering);
+            dxl.writeControlTableItem(PROFILE_ACCELERATION, DXL_ID_STEER, profile_acceleration_steering);
+            dxl.writeControlTableItem(PROFILE_VELOCITY, DXL_ID_STEER, profile_velocity_steering);
 
-        currentSpeed = currentSteer = 0;
+            currentSpeed = currentSteer = 0;
+
+            unlockMutex();
+        }
     }
     void update() {
-        if (newEngineData) {
-            newEngineData = false;
+    }
+
+    void setEnginePower(bool on) {
+        if (lockMutex()) {
+            enginePower = on;
             if (enginePower) {
                 dxl.torqueOn(DXL_ID_SPEED);
                 dxl.torqueOn(DXL_ID_STEER);
-            } else {
+            }
+            else {
                 dxl.torqueOff(DXL_ID_SPEED);
                 dxl.torqueOff(DXL_ID_STEER);
             }
 
-            dxl.setGoalVelocity(DXL_ID_SPEED, utils::safeRemapNorm(engineSpeed, MOTORS_SPEED_MAX), UNIT_RAW);  // +n=CCW, -n=CW
-            currentSpeed = engineSpeed;
-            
-            dxl.setGoalPosition(DXL_ID_STEER, utils::safeRemapNorm(engineSteer, MOTORS_STEER_MAX, MOTORS_STEER_MIDDLE), UNIT_DEGREE);
-            currentSteer = engineSteer;
+            unlockMutex();
         }
     }
 
-    void setEnginePower(bool on) {
-        enginePower = on;
-        newEngineData = true;
-    }
     void setEngineSpeed(float speed) {
-        engineSpeed = speed;
-        newEngineData = true;
+        if (lockMutex()) {
+            currentSpeed = speed;
+            dxl.setGoalVelocity(DXL_ID_SPEED, utils::safeRemapNorm(currentSpeed, MOTORS_SPEED_MAX), UNIT_RAW);  // +n=CCW, -n=CW
+
+            unlockMutex();
+        }
     }
 
     void setEngineSteer(float steer) {
-        engineSteer = steer;
-        newEngineData = true;
+        if (lockMutex()) {
+            currentSteer = steer;
+            dxl.setGoalPosition(DXL_ID_STEER, utils::safeRemapNorm(currentSteer, MOTORS_STEER_MAX, MOTORS_STEER_MIDDLE), UNIT_DEGREE);
+
+            unlockMutex();
+        }
     }
 
     float getEngineSpeed() { return currentSpeed; }
@@ -102,23 +121,47 @@ namespace motors {
     float engineIsMovingForward() { return (currentSpeed >= 0); }
 
     float getBatteryVoltage() {
-        int voltage = dxl.readControlTableItem(PRESENT_INPUT_VOLTAGE, DXL_ID_SPEED);
-        return (voltage / 10.0f);
+        static float voltage = 0;
+
+        if (lockMutex()) {
+            voltage = dxl.readControlTableItem(PRESENT_INPUT_VOLTAGE, DXL_ID_SPEED) / 10.0f;
+            unlockMutex();
+        }
+
+        return voltage;
     }
 
     int getEngineSpeedTemperature() {
-        return dxl.readControlTableItem(PRESENT_TEMPERATURE, DXL_ID_SPEED);
+        static int temperature = 0;
+        if (lockMutex()) {
+            temperature = dxl.readControlTableItem(PRESENT_TEMPERATURE, DXL_ID_SPEED);
+            unlockMutex();
+        }
+        return temperature;
     }
 
     int getEngineSteerTemperature() {
-        return dxl.readControlTableItem(PRESENT_TEMPERATURE, DXL_ID_STEER);
+        static int temperature = 0;
+        if (lockMutex()) {
+            temperature = dxl.readControlTableItem(PRESENT_TEMPERATURE, DXL_ID_STEER);
+            unlockMutex();
+        }
+        return temperature;
     }
 
-
     void collectData() {
-        morphose::json::deviceData["speed"] = getEngineSpeed();
-        morphose::json::deviceData["steer"] = getEngineSteer();
-        morphose::json::deviceData["battery"] = getBatteryVoltage();
+        if (lockMutex()) {
+
+            float speed   = getEngineSpeed();
+            float steer   = getEngineSteer();
+            float battery = getBatteryVoltage();
+            
+            unlockMutex();
+
+            morphose::json::deviceData["speed"]   = speed;
+            morphose::json::deviceData["steer"]   = steer;
+            morphose::json::deviceData["battery"] = battery;
+        }
     }
 
 }  // namespace motors
